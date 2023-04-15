b997d2ab5995a74949c48bfcb4153ec88f20da15
Crypto Cathedral <81304661+crowlsyong@users.noreply.github.com>
2023-04-14 23:58:05 -0500

    more fixes, lots of sillyness
    gonna clean it up soon

--- main.ts
+++ main.ts
@@ -10,5 +10,24 @@ import manifest from "./fresh.gen.ts";
 import twindPlugin from "$fresh/plugins/twind.ts";
 import twindConfig from "./twind.config.ts";
 
-await start(manifest, { plugins: [twindPlugin(twindConfig)] });
-
+await start(manifest, {
+    plugins: [
+      twindPlugin({
+        preflight: {
+          "@font-face": [
+            {
+              fontFamily: "Poppins",
+              src: 'url(/fonts/pxiByp8kv8JHgFVrLEj6Z1JlFd2JQEl8qw.woff2) format("woff2")',
+            },
+            {
+              fontFamily: "Poppins",
+              fontWeight: "bold",
+              src: 'url(/fonts/pxiByp8kv8JHgFVrLDD4Z11lFd2JQEl8qw.woff2) format("woff2")',
+            },
+          ],
+          
+        },
+          ...twindConfig,
+        }),
+    ],
+});
--- twind.config.ts
+++ twind.config.ts
@@ -1,5 +1,10 @@
 import { Options } from "$fresh/plugins/twind.ts";
 
 export default {
+  theme: {
+  fontFamily: {
+    Poppins: ["Poppins", "sans-serif"],
+  },
+},
   selfURL: import.meta.url,
 } as Options;
