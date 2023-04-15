0bf8326cbbf6389070e4c4c25693c414ade82f7e
Crypto Cathedral <81304661+crowlsyong@users.noreply.github.com>
2023-04-14 23:07:15 -0500

    Update _404.tsx

--- routes/_404.tsx
+++ routes/_404.tsx
@@ -1,5 +1,5 @@
 import { ErrorPageProps } from "$fresh/server.ts";
 
-export default function Error500Page({ error }: ErrorPageProps) {
+export default function Error404page({ error }: ErrorPageProps) {
   return <p>404 internal error: {(error as Error).message}</p>;
 }
